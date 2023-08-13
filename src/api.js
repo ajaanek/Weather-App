94954b0d80b28dc84512cefb16d1e6e2090039b6
ajaanek <ajaanek@hotmail.com>
2023-08-13 01:22:09 -0400

    Complete functionality

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '7aaf25cc56msh24e28522ff0e3cap17f145jsnab295c30ab8f',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+export const WEATHER_API_KEY = 'ad4fd4cdfb8d6fdb61329953e45d5abc';
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
